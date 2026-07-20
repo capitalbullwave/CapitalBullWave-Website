159783b8c6524e2b9c40b9ee5e3cc6a73f11fb8a
Akshita <capitalbullwave@gmail.com>
2026-07-20 10:36:25 +0530

    added seo on pages

--- BullWaveCapital-Frontend/src/main.jsx
+++ BullWaveCapital-Frontend/src/main.jsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot, createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(
+    rootElement,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+} else {
+  createRoot(rootElement).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
